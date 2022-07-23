e3f8f20b44091e1aa59687f9592ca4f256a7e086
Elano N. Caitano <nunes.elano14@gmail.com>
2022-07-23 16:48:46 -0300

    Listando tarefas

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -2,7 +2,18 @@ const db = require("../db");
 
 const getAllTasks = async (request, response) => {
     const allTasks = await db.query("SELECT * FROM task");
-    response.json(allTasks.rows);
+    return response.json(allTasks.rows);
+}
+
+const getTask = async (request, response) => {
+    const { id } = request.params;
+    const result = await db.query("SELECT * FROM task WHERE id = $1", [id]);
+
+    if (result.rows.length === 0) {
+        return response.status(400).json({ error: "Task not found"});
+    } 
+    
+    return response.json(result.rows[0]);  
 }
 
 const createTask = async (request, response) => {
@@ -15,5 +26,6 @@ const createTask = async (request, response) => {
 
 module.exports = {
     getAllTasks,
-    createTask
+    createTask,
+    getTask
 }
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 
-const { getAllTasks, createTask } = require("../controllers/task.controller"); 
+const { getAllTasks, getTask, createTask } = require("../controllers/task.controller"); 
 
 const taskRoutes = Router();
 
 taskRoutes.get("/task", getAllTasks);
+
+taskRoutes.get("/task/:id", getTask);
 
 taskRoutes.post("/task", createTask);
 
